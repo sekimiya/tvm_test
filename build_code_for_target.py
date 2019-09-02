5ce346f4df093ec280f592a8b37b0f65865b2a7a
Yuuta Seki <Yuuta.Seki@sony.com>
2019-09-03 02:55:39 +0900

    first commit

---
+++ build_code_for_target.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, print_function
+
+import tvm
+import numpy as np
+
+n = tvm.var('n')
+m = tvm.var('m')
+
+A = tvm.placeholder((m, n), name='A')
+B = tvm.placeholder((m, n), name='B')
+C = tvm.compute((m, n), lambda i, j: A[i, j] * B[i, j], name='C')
+
+s = tvm.create_schedule([C.op])
+
+print("tvm.lower(s, [A, B, C], simple_mode=True)")
+print(tvm.lower(s, [A, B, C], simple_mode=True))
+
+f = tvm.lower(s, [A, B, C], name="test_add")
+
+m = tvm.build(f, target="llvm")
