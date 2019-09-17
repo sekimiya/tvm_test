16dafad86bad02faa89244498fcfd1ad9b42708f
yuuta seki <syakure9@gmail.com>
2019-09-18 00:59:25 +0900

    add working log

--- low_level_custom_pass_test.py
+++ low_level_custom_pass_test.py
@@ -15,8 +15,11 @@ ir  = tvm.lower(sch, [a, b, c], simple_mode=True)
 print("ir")
 print(ir)
 
-
+# find_width8で探索したloopを格納する
 loops = []
+
+test_loop = []
+
 def find_width8(op):
     """ Find all the 'For' nodes whose extent can be divided by 8. """
     if isinstance(op, tvm.stmt.For):
@@ -40,23 +43,71 @@ def vectorize8(op):
 def vectorize(stmt):
     global loops
     # find_width8(self)
+    # 変形対象のstmtをfind_width8()に定義にしたがって、IRを探索して取得 
     tvm.ir_pass.PostOrderVisit(stmt, find_width8)
-    # tvm.ir_pass.PostOrderVisit(stmt, find_width8(self))
 
+    # tvm.ir_pass.PostOrderVisit(stmt, find_width8(self))
+    print ("stmt")
+    print (stmt)
     if not loops:
         return stmt
+    # vectorize8()にしたがってstmtを変形
     stmt = tvm.ir_pass.IRTransform(stmt, None, vectorize8, ['For'])
+    print ("after tansform stmet")
+    print (stmt)
 
     return stmt
 
-def test_loop_transform(stmt):
+def find(op):
+    """ Find all the 'For' nodes whose extent can be divided by 8. """
+    if isinstance(op, tvm.stmt.For):
+        print ("op")
+        print (op)
+        if isinstance(op.extent, tvm.expr.IntImm):
+            loops.append(op)
+
+def test_tranceform(op):
+    """ Split can vectorize the loops found in `find`. """
+    if op in loops:
+        extent = op.extent.value
+        name = op.loop_var.name
+        lo, li = tvm.var(name + '.outer'), tvm.var(name + '.inner')
+        body = tvm.ir_pass.Substitute(op.body, {op.loop_var: lo * 8 + li})
+        body = tvm.make.For(li, 0, 8, tvm.stmt.For.Vectorized, 0, body)
+        body = tvm.make.For(lo, 0, extent // 8, tvm.stmt.For.Serial, 0, body)
+        return body
+    return None
+
+def test(stmt):
     global loops
-    
-    tvm.ir_pass.PostOrderVisit(stmt, find_width8)
+    # find(self)
+    # 変形対象のstmtをfind()に定義にしたがって、IRを探索して取得 
+    tvm.ir_pass.PostOrderVisit(stmt, find)
+
+    # tvm.ir_pass.PostOrderVisit(stmt, find_width8(self))
+    print ("stmt")
+    print (stmt)
+    if not loops:
+        return stmt
+    # test_tranceform()にしたがってstmtを変形
+    stmt = tvm.ir_pass.IRTransform(stmt, None, test_tranceform, ['For'])
+    print ("after tansform stmet")
+    print (stmt)
+
+    return stmt
+
+
+print("test(ir)")
+print(test(ir))
 
 print("vectorize(ir)")
 print(vectorize(ir))
 
+
 print("codegen")
 with tvm.build_config(add_lower_pass=[(1, vectorize)]) as cfg:
+    print(tvm.lower(sch, [a, b, c], simple_mode=True))
+
+print("codegen")
+with tvm.build_config(add_lower_pass=[(1, test)]) as cfg:
     print(tvm.lower(sch, [a, b, c], simple_mode=True))
